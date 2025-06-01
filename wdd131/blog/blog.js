b18af58811e3d5f3b1e2cfae68291670f9271e12
Level3Tree <lvl3tree@gmail.com>
2025-05-31 21:26:14 -0600

    Updates

--- wdd131/blog/blog.js
+++ wdd131/blog/blog.js
@@ -1,3 +1,35 @@
+const articlesContainer = document.querySelector('#articles');
+
+articles.forEach(item => {
+  const article = document.createElement('article');
+  article.classList.add('book-review');
+
+  const template = `
+    <div class="article-content">
+      <div class="article-meta">
+        <div class="article-date">${item.date}</div>
+        <div class="article-ages">${item.ages}</div>
+        <div class="article-genre">${item.genre}</div>
+        <div class="article-rating">${item.stars}</div>
+      </div>
+      <div class="article-body">
+        <div class="article-image">
+          <img src="${item.imgSrc}" alt="${item.imgAlt}">
+        </div>
+        <div class="article-text">
+          <h2>${item.title}</h2>
+          <p>${item.description}</p>
+        </div>
+      </div>
+    </div>
+  `;
+
+  article.innerHTML = template;
+  articlesContainer.appendChild(article);
+});
+
+
+
 const articles = [
 	{
 		id: 1,
@@ -9,7 +41,7 @@ const articles = [
 		imgAlt: 'Book cover for Septimus Heap 1',
 		ages: '10-14',
 		genre: 'Fantasy',
-		stars: '****'
+		stars: '⭐⭐⭐⭐'
 	},
 	{
 		id: 2,
@@ -23,5 +55,18 @@ const articles = [
 		ages: '12-16',
 		genre: 'Fantasy',
 		stars: '⭐⭐⭐⭐'
+	},
+	{
+		id: 3,
+		title: "Belgariad Book One: Pawn of Prophecy",
+		date: "Feb 12, 2022",
+		description:
+		"A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his 'Aunt Pol' and an elderly man calling himself Wolf --a father and daughter granted near-immortality by one of the Gods -- set out on a complex mission.",
+		imgSrc:
+		"https://images-na.ssl-images-amazon.com/images/I/41ZxXA+nInL.jpg",
+		imgAlt: "Book cover for Pawn of Prophecy",
+		ages: "12-16",
+		genre: "Fantasy",
+		stars: "⭐⭐⭐⭐⭐"
 	}
 ]
